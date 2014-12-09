e68d262460e712f379391bcf191f8bb4b15c1cfa
Stino <Linksonder@gmail.com>
2014-12-09 14:58:41 +0100

    Initial

---
+++ MijnPasta/DummyIngredientRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MijnPasta
+{
+    class DummyIngredientRepository : IIngredientenRepository
+    {
+        public IEnumerable<Ingredient> GetAllIngredienten()
+        {
+            return new List<Ingredient>()
+            {
+                new Ingredient{Naam = "Kip", Aantal = 3},
+                new Ingredient{Naam = "Kip bouten", Aantal = 2},
+                new Ingredient{ Naam = "knoflook", Aantal = 10},
+            };
+        }
+
+    }
+}
---
+++ MijnPasta/ReceptViewModel.cs
@@ -0,0 +1,23 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MijnPasta
+{
+    public class ReceptViewModel : ViewModelBase
+    {
+
+        public ObservableCollection<Ingredient> Ingredienten { get; set; }
+
+        public ReceptViewModel(IIngredientenRepository repo)
+        {
+            Ingredienten = new ObservableCollection<Ingredient>(repo.GetAllIngredienten());
+        }
+    }
+
+
+}
